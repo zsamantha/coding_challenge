9f32fe5bb8e1a190a442cde1e462abe77d8115c1
Sam Zielinski <zielin86@msu.edu>
2019-10-24 21:45:14 -0400

    Edited output and extended calculations

--- calc.py
+++ calc.py
@@ -1,17 +1,19 @@
+import math
+
 print("Welcome to the Calculator App")
 print("The following operations are available: + - / *")
 print("Please separate entries with a space. Ex: Use 1 + 2 instead of 1+2")
 print("Type \"Q\" to quit the program.")
 
 result = None
+op = None
+first = True
 
 while(True):
-    result = None
     calc = input().strip().split()
     if calc[0].lower() == "q":
         break
 
-    op = None
     for item in calc:
         if item.isnumeric():
             if result is None:
@@ -38,6 +40,9 @@ while(True):
             op = item
 
     if result is not None:
-        print("= ", result)
+        if result % 1 == 0:
+            print("= ", math.trunc(result))
+        else:
+            print("= {0:.2f}".format(result))
 
 print("Goodbye :)")
